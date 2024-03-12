e2045f3329583734dc2249e609a69f65c0672563
Sobia Fatima <133638285+sobiafatima20@users.noreply.github.com>
2024-03-12 16:34:43 +0500

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,10 @@
+"use strict";
+/*Question # 01 : Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of
+ its author. Your output should look something like the following, including the quotation marks:*/
+console.log('Albert Einstein Once Said,"Life is like riding a bicycle.To keep your balance, you must keep moving."');
+/*Question # 02 : Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a
+variable called famous_person. Then compose your message and store it in a new variable called message. Print
+your message.*/
+let famous_person = "Albert Einstein";
+let message = `${famous_person} Once Said, "Life is like riding a bicycle.To keep your balance, you must keep moving." `;
+console.log(message);
