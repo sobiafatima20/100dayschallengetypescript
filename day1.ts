ce3a9bba50950d53fa4d82e02f269e4199dab555
Sobia Fatima <133638285+sobiafatima20@users.noreply.github.com>
2024-03-12 16:17:30 +0500

    Add files via upload

---
+++ day1.ts
@@ -0,0 +1,13 @@
+//Question # 02 : Personal Message: Store a person’s name in a variable, and print a message to that person. 
+//Your message should be simple, such as, “Hello Eric, would you like to learn some TypeScript today?”
+
+let personsname  : string = "Sobia";
+console.log(`Hello ${personsname}, would you like to learn some TypeScript today?`);
+
+/*Question # 03 : Name Cases: Store a person’s name in a variable, and then print that person’s name in 
+lowercase, uppercase, and titlecase.*/
+
+let name1 : string = "Sobia";
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
