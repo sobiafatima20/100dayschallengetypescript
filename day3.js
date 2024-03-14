a18287bbf1f59d04dc973f439c3531da87074f3f
Sobia Fatima <133638285+sobiafatima20@users.noreply.github.com>
2024-03-14 13:13:50 +0500

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,30 @@
+"use strict";
+/*Question # 01 : Number Eight: Write addition, subtraction, multiplication, and division operations that
+each result in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+//Addition
+let num1 = 2;
+let num2 = 6;
+console.log(num1 + num2);
+//Subtraction
+let num3 = 12;
+let num4 = 4;
+console.log(num3 - num4);
+//Multiplication
+let num5 = 2;
+let num6 = 4;
+console.log(num5 * num6);
+//Division
+let num7 = 16;
+let num8 = 2;
+console.log(num7 / num8);
+/*Question # 02 : You should create four lines that look like this: console.log(5 + 3) Your output should
+simply be four lines with the number 8 appearing once on each line.*/
+console.log(5 + 3);
+console.log(12 - 4);
+console.log(2 * 4);
+console.log(16 / 2);
+/*Question # 03 : Favorite Number: Store your favorite number in a variable. Then, using that variable,
+create a message that reveals your favorite number. Print that message. */
+let favoriteNumber = 7;
+let message1 = `My Favorite Number is ${favoriteNumber}`;
+console.log(message1);
